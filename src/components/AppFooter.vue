1aa4fead971a7f0a31f642bfc9c7b99abd82cc79
MarcinSufa <sufa.marcin@gmail.com>
2024-05-22 22:00:17 +0200

    fixed footer position on mobile

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray:50 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+  <footer class="md:fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray:50 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
     <div class='container flex mx-auto justify-between items-center'>
     <span class="text-sm sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" class="hover:underline text-blue-600">Social Hive</a>. All Rights Reserved.
     </span>
